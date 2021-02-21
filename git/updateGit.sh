d945b03e7db60e60733b0b86b3e842db7dc09023
zongou <zongou@outlook.com>
2021-02-22 07:34:58 +0800

    update

--- git/updateGit.sh
+++ git/updateGit.sh
@@ -1,1 +1,6 @@
+if [ ! -f ~/.gitconfig ];then
+    git config --global user.name zongou
+    git config --global user.email zongou@outlook.com
+    git config credential.helper store
+fi
 echo "cwd: "$(pwd) && git add -A && git commit -m update && git push
